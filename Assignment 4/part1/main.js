65a630c1d0ab190a363c970a0572f867be285ea4
ARTIFICI4L <aman.arshad@dcmail.ca>
2024-04-01 13:45:43 -0400

    Assignment 4 part 1 complete

--- Assignment 4/part1/main.js
+++ Assignment 4/part1/main.js
@@ -11,3 +11,32 @@ const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. W
 const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
 const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
 const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+
+randomize.addEventListener('click', result);
+
+function result() {
+  let newStory = storyText;
+  let xItem = randomValueFromArray(insertX);
+  let yItem = randomValueFromArray(insertY);
+  let zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replace(':insertx:', xItem).replace(':insertx:', xItem); // :insertx: appears twice
+  newStory = newStory.replace(':inserty:', yItem);
+  newStory = newStory.replace(':insertz:', zItem);
+
+  if(customName.value !== '') {
+    const name = customName.value;
+    newStory = newStory.replace('Bob', name);
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300/14) + ' stone';
+    const temperature =  Math.round((94 - 32) * 5 / 9) + ' centigrade';
+
+    newStory = newStory.replace('94 fahrenheit', temperature);
+    newStory = newStory.replace('300 pounds', weight);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
