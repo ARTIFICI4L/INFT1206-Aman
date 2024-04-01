477a77c2e74c0300c664280b371edbb6f9a9fb6e
ARTIFICI4L <aman.arshad@dcmail.ca>
2024-04-01 15:21:46 -0400

    Assignment 4 part 2 Declaring image filename and alt text

--- Assignment 4/part2/gallery-start/gallery-start/main.js
+++ Assignment 4/part2/gallery-start/gallery-start/main.js
@@ -5,9 +5,16 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageArray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
-
+const alts = {
+  'pic1.jpg' : 'Closeup of a human eye',
+  'pic2.jpg' : 'Closeup of Vincent van Gogh painting',
+  'pic3.jpg' : 'Purple and white flowers',
+  'pic4.jpg' : 'Painting from ancient egypt',
+  'pic5.jpg' : 'Butterfly on a leaf'
+}  
 /* Looping through images */
 
 const newImage = document.createElement('img');
